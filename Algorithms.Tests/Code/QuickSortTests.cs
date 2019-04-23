cb6d5a72619bb7e8d823078e62d1b8126b12ead7
Simonova.Elena2 <Simonova.Elena2@WB-5063>
2019-04-23 20:08:36 +0300

    quick sort part

---
+++ Algorithms.Tests/Code/QuickSortTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Algorithms.Code;
+using System.Linq;
+
+namespace Algorithms.Tests.Code
+{
+    public class QuickSortTests
+    {
+        [Fact]
+        public void RecursiveSumTest()
+        {
+            var sorter = new QuickSort();
+            var list = new[] { 1, 2, 3, 4 };
+
+            var resultExpected = 0;
+            var resultActual = sorter.RecursiveSum(list);
+
+            foreach (var item in list)
+                resultExpected += item;
+
+            Assert.Equal(resultExpected, resultActual);
+        }
+
+        [Fact]
+        public void ElementsCounterTest()
+        {
+            var sorter = new QuickSort();
+            var list = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var resultExpected = list.Length;
+            var resultActual = sorter.ElementsCounter(list);
+
+            Assert.Equal(resultExpected, resultActual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new[] { 1 })]
+        [InlineData(new[] { 3, 2})]
+        [InlineData(new[] { 1, 2, 6, 7, 8, 3, 4, 9, 5 })]
+        public void FindMaxTest(int[] arr)
+        {
+            var sorter = new QuickSort();
+
+            if (!arr.Any())
+                Assert.Throws<ArgumentException>(() => sorter.FindMax(arr));
+            else
+            {
+                var resultExpected = arr.Max();
+                var resultActual = sorter.FindMax(arr);
+
+                Assert.Equal(resultExpected, resultActual);
+            }
+        }
+    }
+}
--- Algorithms/Code/QuickSort.cs
+++ Algorithms/Code/QuickSort.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Algorithms.Code
 {
     public class QuickSort
     {
+        public int LoopSum(IEnumerable<int> numbers)
+        {
+            var total = 0;
+
+            foreach (var number in numbers)
+                total += number;
+
+            return total;
+        }
+
+        public int RecursiveSum(IEnumerable<int> list)
+        {
+            if (!list.Any())
+                return 0;
+
+            return list.Take(1).First() + RecursiveSum(list.Skip(1));
+        }
+
+        public int ElementsCounter(IEnumerable<int> list)
+        {
+            if (!list.Any())
+                return 0;
+
+            return 1 + ElementsCounter(list.Skip(1));
+        }
+
+        public int FindMax(IEnumerable<int> list)
+        {
+            if (!list.Any())
+                throw new ArgumentException(nameof(list));
+
+            if (list.Count() == 1)
+                return list.First();
+
+            if (list.Count() == 2)
+                return list.First() > list.Last() ? list.First() : list.Last();
+
+            var subMax = FindMax(list.Skip(1));
+            return list.First() > subMax ? list.First() : subMax;
+        }
     }
 }
