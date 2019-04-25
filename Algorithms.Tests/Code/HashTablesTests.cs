330c5b5c205293f79e2c5ac1b9875cd804e42692
Simonova.Elena2 <Simonova.Elena2@WB-5063>
2019-04-25 14:28:19 +0300

    hash table

---
+++ Algorithms.Tests/Code/HashTablesTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Code;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.Tests.Code
+{
+    public class HashTablesTests
+    {
+        HashTables _hashTables = new HashTables();
+
+        [Fact]
+        public void CheckVoterTest()
+        {
+            var voter1 = "Tom";
+            var voter2 = "Mike";
+
+            var resultActual = _hashTables.CheckVoter(voter1);
+            Assert.True(resultActual);
+
+            resultActual = _hashTables.CheckVoter(voter2);
+            Assert.True(resultActual);
+
+            resultActual = _hashTables.CheckVoter(voter1);
+            Assert.False(resultActual);
+        }
+    }
+}
---
+++ Algorithms/Code/HashTables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Code
+{
+    public class HashTables
+    {
+        Dictionary<string, bool> _voted;
+
+        public bool CheckVoter(string name)
+        {
+            if (_voted == null)
+                _voted = new Dictionary<string, bool>();
+
+            if (_voted.ContainsKey(name))
+                return false;
+            else
+            {
+                _voted.Add(name, true);
+                return true;
+            }
+        }
+    }
+}
