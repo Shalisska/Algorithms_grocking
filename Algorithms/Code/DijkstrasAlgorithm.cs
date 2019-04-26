d9720a316e87f7bb53bb80e69dcd8e39910893fb
Simonova.Elena2 <Simonova.Elena2@WB-5063>
2019-04-26 18:13:24 +0300

    dijkstra tables

--- Algorithms/Code/DijkstrasAlgorithm.cs
+++ Algorithms/Code/DijkstrasAlgorithm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Algorithms.Code
@@ -7,41 +8,73 @@ namespace Algorithms.Code
     public class DijkstrasAlgorithm
     {
         private Dictionary<string, Dictionary<string, double>> _graph;
+        private Dictionary<string, double> _costs;
+        private Dictionary<string, string> _parents;
+        private List<string> _processed;
 
         float _infinity => float.PositiveInfinity;
 
-        public Dictionary<string, Dictionary<string, double>> Graph
-        {
-            get
-            {
-                if (_graph == null)
-                    SetGraph();
+        public Dictionary<string, Dictionary<string, double>> Graph { get => _graph ?? SetGraph(); set => _graph = value; }
+        public Dictionary<string, double> Costs { get => _costs ?? SetCosts(); set => _costs = value; }
+        public Dictionary<string, string> Parents { get => _parents ?? SetParents(); set => _parents = value; }
+        public List<string> Processed { get => _processed ?? new List<string>(); set => _processed = value; }
 
-                return _graph;
-            }
-            set => _graph = value;
+        Dictionary<string, Dictionary<string, double>> SetGraph()
+        {
+            var graph = new Dictionary<string, Dictionary<string, double>>
+            {
+                ["start"] = new Dictionary<string, double>
+                {
+                    ["a"] = 6,
+                    ["b"] = 2
+                },
+
+                ["a"] = new Dictionary<string, double>
+                {
+                    ["fin"] = 1
+                },
+
+                ["b"] = new Dictionary<string, double>
+                {
+                    ["a"] = 3,
+                    ["fin"] = 5
+                },
+
+                ["fin"] = new Dictionary<string, double>()
+            };
+
+            return graph;
         }
 
-        void SetGraph()
+        Dictionary<string, double> SetCosts()
         {
-            _graph["start"] = new Dictionary<string, double>
+            var costs = new Dictionary<string, double>
             {
                 ["a"] = 6,
-                ["b"] = 2
+                ["b"] = 2,
+                ["fin"] = _infinity
             };
 
-            _graph["a"] = new Dictionary<string, double>
+            return costs;
+        }
+
+        Dictionary<string, string> SetParents()
+        {
+            var parents = new Dictionary<string, string>
             {
-                ["fin"] = 1
+                ["a"] = "start",
+                ["b"] = "start",
+                ["fin"] = string.Empty
             };
 
-            _graph["b"] = new Dictionary<string, double>
-            {
-                ["a"] = 3,
-                ["fin"] = 5
-            };
+            return parents;
+        }
 
-            _graph["fin"] = new Dictionary<string, double>();
+        KeyValuePair<string,double> FindLowestCostNode()
+        {
+            var min = Costs.Select(c => c.Value).Min();
+            var node = Costs.FirstOrDefault(n => !Processed.Contains(n.Key) && n.Value == min);
+            return node;
         }
     }
 }
